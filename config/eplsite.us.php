6f1a485d9cfc77cb0d3017774af01efcd8d0a5ad
Valerimakaridze <valerianmakaridze@gmail.com>
2025-11-24 22:48:48 +0400

    Add configuration files for Eplsite and SportLemons; update sports categories and SEO metadata for improved site visibility and organization.

---
+++ config/eplsite.us.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'site_name' => 'Eplsite',
+    
+    'logo' => '',
+    
+    'theme' => [
+        'primary_color' => '#ff4d00',
+        'secondary_color' => '#ff1100',
+    ],
+    
+    'seo' => [
+        'title' => 'Eplsite - Live Sports Streaming | Watch Games Online Free',
+        'description' => 'Watch live sports streaming online free. Football, Basketball, Tennis and more. Eplsite offers the best live sports streams in HD quality.',
+        'keywords' => 'live sports, sports streaming, watch sports online, free sports streams, football streaming, basketball live, Eplsite',
+    ],
+    
+    'language' => 'en',
+    
+    'contact' => [
+        'email' => 'contact@eplsite.us',
+        'support' => 'support@eplsite.us',
+    ],
+    
+    'social' => [
+        'twitter' => 'https://twitter.com/eplsite',
+        'facebook' => 'https://facebook.com/eplsite',
+    ],
+];
